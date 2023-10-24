b88caadf13a15eeaa81bf579e04741caabd77ebd
Nikita.Zmanovskiy <72348175+test212366@users.noreply.github.com>
2023-10-24 21:29:52 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,194 @@
+import * as THREE from 'three'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader' 
+import GUI from 'lil-gui'
+import gsap from 'gsap'
+import fragmentShader from './shaders/fragment.glsl'
+import vertexShader from './shaders/vertex.glsl'
+ 
+import {EffectComposer} from 'three/examples/jsm/postprocessing/EffectComposer'
+import {RenderPass} from 'three/examples/jsm/postprocessing/RenderPass'
+import {ShaderPass} from 'three/examples/jsm/postprocessing/ShaderPass'
+import {GlitchPass} from 'three/examples/jsm/postprocessing/GlitchPass'
+
+import two from './two.jpg'
+
+export default class Sketch {
+	constructor(options) {
+		
+		this.scene = new THREE.Scene()
+		
+		this.container = options.dom
+		
+		this.width = this.container.offsetWidth
+		this.height = this.container.offsetHeight
+		
+		
+		// // for renderer { antialias: true }
+		this.renderer = new THREE.WebGLRenderer({ antialias: true })
+		this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+		this.renderTarget = new THREE.WebGLRenderTarget(this.width, this.height)
+		this.renderer.setSize(this.width ,this.height )
+		this.renderer.setClearColor(0x00000, 1)
+		this.renderer.useLegacyLights = true
+		this.renderer.outputEncoding = THREE.sRGBEncoding
+ 
+
+		 
+		this.renderer.setSize( window.innerWidth, window.innerHeight )
+
+		this.container.appendChild(this.renderer.domElement)
+ 
+
+
+		this.camera = new THREE.PerspectiveCamera( 70,
+			 this.width / this.height,
+			 0.01,
+			 10
+		)
+ 
+		this.camera.position.set(0, 0, 2) 
+		this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+		this.time = 0
+
+
+		this.dracoLoader = new DRACOLoader()
+		this.dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/')
+		this.gltf = new GLTFLoader()
+		this.gltf.setDRACOLoader(this.dracoLoader)
+
+		this.isPlaying = true
+
+		this.addObjects()		 
+		this.resize()
+		this.render()
+		this.setupResize()
+
+ 
+	}
+
+	settings() {
+		let that = this
+		this.settings = {
+			progress: 0
+		}
+		this.gui = new GUI()
+		this.gui.add(this.settings, 'progress', 0, 1, 0.01)
+	}
+
+	setupResize() {
+		window.addEventListener('resize', this.resize.bind(this))
+	}
+
+	resize() {
+		this.width = this.container.offsetWidth
+		this.height = this.container.offsetHeight
+		this.renderer.setSize(this.width, this.height)
+		this.camera.aspect = this.width / this.height
+
+
+		this.imageAspect = 853/1280
+		let a1, a2
+		if(this.height / this.width > this.imageAspect) {
+			a1 = (this.width / this.height) * this.imageAspect
+			a2 = 1
+		} else {
+			a1 = 1
+			a2 = (this.height / this.width) / this.imageAspect
+		} 
+
+
+		// this.material.uniforms.resolution.value.x = this.width
+		// this.material.uniforms.resolution.value.y = this.height
+		// this.material.uniforms.resolution.value.z = a1
+		// this.material.uniforms.resolution.value.w = a2
+
+		this.camera.updateProjectionMatrix()
+
+
+
+	}
+
+
+	addObjects() {
+		let that = this
+		this.loader = new THREE.TextureLoader()
+		this.loader.load(two, texture => {
+
+			this.material = new THREE.PointsMaterial({
+				size: 20,
+				map: texture,
+				alphaTest: 0.5
+			})
+			this.geometry = new THREE.Geometry()
+			
+			
+			for (let i = 0; i < 50; i++) {
+				this.geometry.vertices.push(new THREE.Vector3(Math.random() * 100, Math.random() * 100, Math.random() * 100))
+
+				
+			}
+			
+			
+			this.plane = new THREE.Points(this.geometry, this.material)
+	
+			this.scene.add(this.plane)
+		})
+
+ 
+		// this.material = new THREE.PointsMaterial({
+		// 	extensions: {
+		// 		derivatives: '#extension GL_OES_standard_derivatives : enable'
+		// 	},
+		// 	side: THREE.DoubleSide,
+		// 	uniforms: {
+		// 		time: {value: 0},
+		// 		resolution: {value: new THREE.Vector4()}
+		// 	},
+		// 	vertexShader,
+		// 	fragmentShader
+		// })
+ 
+	}
+
+
+
+	addLights() {
+		const light1 = new THREE.AmbientLight(0xeeeeee, 0.5)
+		this.scene.add(light1)
+	
+	
+		const light2 = new THREE.DirectionalLight(0xeeeeee, 0.5)
+		light2.position.set(0.5,0,0.866)
+		this.scene.add(light2)
+	}
+
+	stop() {
+		this.isPlaying = false
+	}
+
+	play() {
+		if(!this.isPlaying) {
+			this.isPlaying = true
+			this.render()
+		}
+	}
+
+	render() {
+		if(!this.isPlaying) return
+		this.time += 0.05
+		// this.material.uniforms.time.value = this.time
+		 
+		//this.renderer.setRenderTarget(this.renderTarget)
+		this.renderer.render(this.scene, this.camera)
+		//this.renderer.setRenderTarget(null)
+ 
+		requestAnimationFrame(this.render.bind(this))
+	}
+ 
+}
+new Sketch({
+	dom: document.getElementById('container')
+})
+ 
